fdd27e9f11a714be7f3e3bc2f566cfa7da241de9
Sean O'Toole <sotoole@signal.co>
2014-10-24 11:38:58 -0500

    started replacement of regexp with sre2

--- wiki.go
+++ wiki.go
@@ -7,12 +7,14 @@ import (
         "log"
         "net"
         "net/http"
-        "regexp"
+        // "regexp"
+        "code.google.com/p/sre2/sre2"
         "errors"
 )
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+// var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")                   // change this to sre2.MustParse(...)
+var validPath = sre2.MustParse("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 var addr = flag.Bool("addr", false, "find open address and print to final-port.txt")
 
 type Page struct {
@@ -41,11 +43,6 @@ func main() {
         http.ListenAndServe(":8080", nil)
 }
 
-func (p *Page) save() error { //p is a pointer to Page
-        filename := p.Title + ".txt"
-        return ioutil.WriteFile(filename, p.Body, 0600) // 0600 is an octal integer literal signifies that the file create has read-write permissions current user only (unix).
-}
-
 func loadPage(title string) (*Page, error) {
         filename := title + ".txt"
         body, err := ioutil.ReadFile(filename)
@@ -53,6 +50,22 @@ func loadPage(title string) (*Page, error) {
                 return nil, err
         }
         return &Page{Title: title, Body: body}, nil
+}
+
+func (p *Page) save() error {
+        filename := p.Title + ".txt"
+        return ioutil.WriteFile(filename, p.Body, 0600) // 0600 is an octal integer literal signifies that the file create has read-write permissions current user only (unix).
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+        u := r.URL.Path
+        // m := validPath.FindStringSubmatch(u)           // regexp
+        m := validPath.MatchIndex(u)
+        if m == nil {
+                http.NotFound(w, r)
+                return "", errors.New("Invalid Page Title")
+        }
+        return string(u(m[2])), nil // The title is the second subexpression of the regular expression output. Needs to change to be used with sre2
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
@@ -85,12 +98,14 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 
 func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
         return func(w http.ResponseWriter, r *http.Request) {
-                m := validPath.FindStringSubmatch(r.URL.Path)
+                u := r.URL.Path
+                // m := validPath.FindStringSubmatch(u)   // regexp
+                m := validPath.MatchIndex(u)
                 if m == nil {
                         http.NotFound(w, r)
                         return
                 }
-                fn(w, r, m[2])
+                fn(w, r, string(u(m[2]))) // Will return an int with sre2.
         }
 }
 
@@ -101,13 +116,4 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
                 http.Error(w, err.Error(), http.StatusInternalServerError)
                 return
         }
-}
-
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-        m := validPath.FindStringSubmatch(r.URL.Path)
-        if m == nil {
-                http.NotFound(w, r)
-                return "", errors.New("Invalid Page Title")
-        }
-        return m[2], nil // The title is the second subexpression.
 }
